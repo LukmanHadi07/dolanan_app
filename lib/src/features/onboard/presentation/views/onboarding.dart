8c7a072737fc3e8a06529689e793ea8acf2f7b1d
Lukman Hadi <lukmanhadi2000@gmail.com>
2025-01-16 00:36:53 +0700

    add screen onboarding

--- lib/src/features/onboard/presentation/views/onboarding.dart
+++ lib/src/features/onboard/presentation/views/onboarding.dart
@@ -1,10 +1,117 @@
+import 'dart:async';
+import 'package:dulinan/src/features/onboard/presentation/widgets/slide_data.dart';
+import 'package:dulinan/src/features/onboard/presentation/widgets/slide_items.dart';
+import 'package:dulinan/src/features/onboard/presentation/widgets/slidedots.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class OnBoarding extends StatelessWidget {
+class OnBoarding extends StatefulWidget {
   const OnBoarding({super.key});
 
   @override
+  State<OnBoarding> createState() => _OnBoardingState();
+}
+
+class _OnBoardingState extends State<OnBoarding> {
+  int _currentPage = 0;
+  final PageController _pageController = PageController(initialPage: 0);
+
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(const Duration(seconds: 5), (Timer timer) {
+      if (_currentPage < 2) {
+        _currentPage++;
+      } else {
+        _currentPage = 0;
+      }
+
+      _pageController.animateToPage(
+        _currentPage,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeIn,
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  _onPageChanged(int index) {
+    setState(() {
+      _currentPage = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Expanded(
+              child: Stack(
+                children: [
+                  PageView.builder(
+                    scrollDirection: Axis.horizontal,
+                    controller: _pageController,
+                    onPageChanged: _onPageChanged,
+                    itemCount: slideList.length,
+                    itemBuilder: (context, index) {
+                      return SlideItems(index: index);
+                    },
+                  ),
+                  Positioned(
+                    bottom: 100,
+                    left: 0,
+                    right: 0,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        for (int i = 0; i < slideList.length; i++)
+                          Slidedots(isActive: i == _currentPage),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 20,
+                    left: 0,
+                    right: 0,
+                    child: Center(
+                      child: SizedBox(
+                        width: 400,
+                        height: 60,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue,
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 50, vertical: 15),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                          ),
+                          child: Text(
+                            'Get Started',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16.sp,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/src/features/onboard/presentation/widgets/slide_items.dart
@@ -0,0 +1,50 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:dulinan/src/features/onboard/presentation/widgets/slide_data.dart';
+import 'package:flutter/material.dart';
+
+class SlideItems extends StatelessWidget {
+  final int index;
+  const SlideItems({
+    super.key,
+    required this.index,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 750,
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.only(
+              bottomLeft: Radius.circular(25),
+              bottomRight: Radius.circular(25),
+            ),
+            image: DecorationImage(
+                image: AssetImage(
+                  slideList[index].image,
+                ),
+                fit: BoxFit.cover),
+          ),
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        Text(slideList[index].title,
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+                  fontWeight: FontWeight.bold,
+                )),
+        const SizedBox(
+          height: 15,
+        ),
+        Text(slideList[index].description,
+            textAlign: TextAlign.center,
+            style: Theme.of(context)
+                .textTheme
+                .bodyLarge!
+                .copyWith(fontWeight: FontWeight.normal)),
+      ],
+    );
+  }
+}
