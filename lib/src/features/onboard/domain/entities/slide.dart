236a3db5f4fef295059d9c3d835ae5028c26e0c9
Lukman Hadi <lukmanhadi2000@gmail.com>
2025-01-16 00:36:35 +0700

    add entities slide.dart for slide function

---
+++ lib/src/features/onboard/domain/entities/slide.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Slide {
+  final String image;
+  final String title;
+  final String description;
+  Slide({
+    required this.image,
+    required this.title,
+    required this.description,
+  });
+}
