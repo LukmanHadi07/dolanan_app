712742c69616c8dae2c7a9358776aefba5714dd4
Lukman Hadi <lukmanhadi2000@gmail.com>
2025-01-20 00:07:03 +0700

    add class image_constants for handle image

---
+++ lib/src/core/images_const/image_constants.dart
@@ -0,0 +1,10 @@
+class ImageConstants {
+  ImageConstants._init();
+  static ImageConstants? _instace;
+  static ImageConstants get instance => _instace ??= ImageConstants._init();
+  String get textBanner => toPng("banner");
+
+  String toJpg(String name) => "assets/images/$name.jpg";
+  String toSvg(String name) => "assets/images/$name.svg";
+  String toPng(String name) => "assets/images/$name.png";
+}
