86e4a0aa622c610ce3da8bc65f788322ab23ca7c
Lukman Hadi <lukmanhadi2000@gmail.com>
2025-01-20 00:09:10 +0700

    add home screen

--- lib/src/features/home/presentation/views/home.dart
+++ lib/src/features/home/presentation/views/home.dart
@@ -1,10 +1,313 @@
+import 'package:dulinan/src/core/images_const/image_constants.dart';
+import 'package:dulinan/src/core/theme/color.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late ScrollController _scrollController;
+  final double _scrollSpeed = 50.0;
+  bool _isScrolling = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    startScrolling();
+  }
+
+  void startScrolling() {
+    Future.delayed(const Duration(milliseconds: 50), () {
+      if (_scrollController.hasClients && _isScrolling) {
+        double maxScroll = _scrollController.position.maxScrollExtent;
+        double currentScroll = _scrollController.offset;
+
+        if (currentScroll >= maxScroll) {
+          _scrollController.jumpTo(0.0);
+        } else {
+          _scrollController.animateTo(
+            currentScroll + 1,
+            duration: Duration(milliseconds: (1000 / _scrollSpeed).round()),
+            curve: Curves.linear,
+          );
+        }
+        startScrolling();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _isScrolling = false;
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 45,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _headerProfile(),
+            _titleLabel(),
+            _rowCircle(),
+            _bestDestination(),
+            _destinationList()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _headerProfile() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          height: 40,
+          width: 150,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(25),
+            color: Colors.grey.shade300,
+          ),
+          child: Row(
+            children: [
+              Container(
+                height: 40,
+                width: 40,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(width: 15),
+              const Text('Lukman')
+            ],
+          ),
+        ),
+        Container(
+          height: 40,
+          width: 40,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.grey.shade300,
+          ),
+          child: const Icon(
+            Icons.notifications_active_outlined,
+            color: Colors.white,
+            size: 20,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _titleLabel() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 15),
+      child: Image.asset(
+        ImageConstants.instance.textBanner,
+        width: 350.w,
+      ),
+    );
+  }
+
+  Widget _rowCircle() {
+    final List<Widget> circles = List.generate(
+      8,
+      (index) => Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+        ),
+        child: Column(
+          children: [
+            Container(
+              height: 50,
+              width: 50,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 5),
+            const SizedBox(
+              width: 50,
+              child: Text(
+                'Wisata',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 12,
+                ),
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+
+    return Container(
+      height: 100,
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: ListView(
+        controller: _scrollController,
+        scrollDirection: Axis.horizontal,
+        physics: const NeverScrollableScrollPhysics(),
+        children: [
+          ...circles,
+          ...circles,
+        ],
+      ),
+    );
+  }
+
+  Widget _bestDestination() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 30),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            'Best Destination',
+            style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                fontFamily: GoogleFonts.poppins(fontWeight: FontWeight.w500)
+                    .fontFamily),
+          ),
+          InkWell(
+            onTap: () {},
+            child: Text(
+              'View All',
+              style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                  fontFamily: GoogleFonts.poppins(fontWeight: FontWeight.w300)
+                      .fontFamily,
+                  color: AppColors.primaryColor),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _destinationList() {
+    return Container(
+      height: 400,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 4,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: EdgeInsets.only(
+              left: index == 0 ? 20 : 15,
+              right: index == 3 ? 20 : 15,
+            ),
+            child: _cardDestination(),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _cardDestination() {
+    return Container(
+      height: 400,
+      width: 268,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.shade300,
+            blurRadius: 10,
+            offset: const Offset(0, 5),
+          )
+        ],
+        color: Colors.white,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 20),
+        child: Column(
+          children: [
+            Container(
+              height: 286,
+              width: 240,
+              decoration: BoxDecoration(
+                color: Colors.grey.shade300,
+                borderRadius: BorderRadius.circular(25),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Pantai',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                        size: 20,
+                      ),
+                      SizedBox(width: 5),
+                      Text(
+                        '4.5',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.location_on,
+                        color: Colors.grey,
+                        size: 20,
+                      ),
+                      SizedBox(width: 5),
+                      Text(
+                        'Denpasar Bali',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
